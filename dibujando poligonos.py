4d4d6b5644e50f1db08f7257b96577ca023379fc
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2018-02-02 12:11:22 +0100

    Update dibujando poligonos.py

--- dibujando poligonos.py
+++ dibujando poligonos.py
@@ -2,12 +2,14 @@ print("DIBUJANDO POLIGONOS")
 from VALID import OKI, OK, ns
 from turtle import Turtle
 import subprocess
+
 def set_color(cf):
     try:
         t.screen.bgcolor(cf)
     except:
         cf=set_color(input("Color no valido: "))
     return cf
+        
 def set_color2(ct):
     try:
         t.color(ct)
@@ -26,23 +28,25 @@ if preg==("s"):
         print("DIBUJANDO POLIGONOS")
 
         cf=set_color(input("Indica el color del fondo: "))
-        ct=set_color2(input("Indica el color de la línea: "))
-        while ct==cf or ct==(""):
-            ct=set_color2(input("Especifica un color diferente para la línea: "))
+        ct=set_color2(input("Indica el color del contorno: "))
+        while ct==cf:
+            ct=set_color2(input("Especifica un color diferente para el contorno: "))
         qc=ns(input("¿Especificar coordenadas?: "))
         if qc==("s"):
-            cordenada_x=OKI(input("Introduce coordenada\'x\': "))
-            cordenada_y=OKI(input("Introduce coordenada\'y\': "))
-            t.color(cf)
-            t.setx(cordenada_x)
-            t.sety(cordenada_y)
-        t.screen.bgcolor(cf)
-        t.color(ct)
+            coordenada_x=OKI(input("Introduce coordenada\'x\': "))
+            coordenada_y=OKI(input("Introduce coordenada\'y\': "))
+            t.color(cf)#HACEMOS EL COLOR DE LA LINEA IGUAL AL COLOR DEL FONDO
+            t.setx(coordenada_x)
+            t.sety(coordenada_y)
+            t.color(ct)#RESTABLECEMOS EL COLOR PARA LA LINEA
+
+        #t.screen.bgcolor(cf)
+        #t.color(ct)
         gt=OK(input("Indica el grosor de la línea: "))
         t.pensize(gt)
         n=OKI(input("Indica el número de lados del polígono: "))
-        ln=OK(input("Indica la longitud de la línea: "))
-        ang=180-(((n-2)/n)*180)
+        ln=OK(input("Indica la longitud del lado: "))
+        ang=180-(((n-2)/n)*180)#CALCULAMOS EL ANGULO DE GIRO DEL PUNTERO
         for i in range(n):
             t.left(ang)
             t.fd(ln)
@@ -50,6 +54,6 @@ if preg==("s"):
         if conti==("s"):
             t.clear()
             subprocess.call(["cmd.exe","/C","cls"])
+        else:
+            break
     t.screen.mainloop()
-
-
